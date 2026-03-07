97e68c03b497ad13635e85db1df75c52addd2869
S Sai Krishna <saikrishnamac@Ss-MacBook-Air.local>
2026-03-07 10:20:23 +0530

    Fix Vercel pg module resolution via dialectModule

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,12 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+import pg from 'pg';
 
 dotenv.config();
-// console.log(dotenv.config());
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  dialectModule: pg,
   logging: false,
 });
 
